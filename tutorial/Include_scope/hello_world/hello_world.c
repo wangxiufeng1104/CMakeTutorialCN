d15d40e79b7ad13d2f2a76dd1ba25e5dff2b0027
王秀峰 <33338160+wangxiufeng1104@users.noreply.github.com>
2023-06-05 03:00:43 +0000

    【hello】add interface and private example

---
+++ tutorial/Include_scope/hello_world/hello_world.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "hello.h"
+
+void hello_world()
+{
+    hello();
+    printf("world\n");
+}
---
+++ tutorial/Include_scope/hello_world/hello_world.h
@@ -0,0 +1,4 @@
+#ifndef __HELLO_WORLD_H
+#define __HELLO_WORLD_H
+void hello_world();
+#endif
